0b6af2ef1abce2c54c5f7e1a68bdd491d70059e8
lynnemunini <muninilynne65@gmail.com>
2022-11-25 17:58:10 +0300

    Make illustration overlap with card

--- app/src/main/java/com/grayseal/triviaapp/screens/QuizScreen.kt
+++ app/src/main/java/com/grayseal/triviaapp/screens/QuizScreen.kt
@@ -4,39 +4,38 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme.colors
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.sharp.Cancel
 import androidx.compose.material.icons.sharp.Close
-import androidx.compose.material.icons.sharp.Edit
-import androidx.compose.material.icons.sharp.Remove
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.BlurredEdgeTreatment
-import androidx.compose.ui.draw.blur
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.zIndex
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 import androidx.navigation.NavController
 import com.grayseal.triviaapp.R
 import com.grayseal.triviaapp.navigation.QuizScreens
 import com.grayseal.triviaapp.ui.theme.sonoFamily
 import com.grayseal.triviaapp.utils.shakeClickEffect
+import kotlinx.coroutines.NonDisposableHandle.parent
+
 
 @Composable
-fun QuizScreen(navController: NavController){
-    Column(modifier = Modifier
-        .fillMaxSize()
-        .background(color = colors.secondary)
-        ) {
+fun QuizScreen(navController: NavController) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = colors.secondary)
+    ) {
         CancelButton(navController = navController)
         QuestionCard()
     }
@@ -44,13 +43,16 @@ fun QuizScreen(navController: NavController){
 }
 
 @Composable
-fun CancelButton(navController: NavController){
+fun CancelButton(navController: NavController) {
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(top = 20.dp), horizontalArrangement = Arrangement.Start, verticalAlignment = Alignment.Top){
+            .padding(top = 20.dp),
+        horizontalArrangement = Arrangement.Start,
+        verticalAlignment = Alignment.Top
+    ) {
         FilledTonalButton(
-            onClick = {navController.navigate(route = QuizScreens.QuizScreen.name)},
+            onClick = { navController.navigate(route = QuizScreens.QuizScreen.name) },
             modifier = Modifier.shakeClickEffect(),
             colors = ButtonDefaults.buttonColors(containerColor = colors.primary),
             shape = RoundedCornerShape(100.dp),
@@ -62,32 +64,59 @@ fun CancelButton(navController: NavController){
                 tint = Color.White
             )
         }
-}
+    }
 }
 
+@Preview(showBackground = true)
 @Composable
-fun QuestionCard(){
-    Column(verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
-        ElevatedCard(
-            colors = CardDefaults.cardColors(containerColor = Color.White),
-            shape = RoundedCornerShape(10.dp),
-            elevation = CardDefaults.cardElevation(10.dp)
+fun QuestionCard() {
+    ConstraintLayout(
+        modifier = Modifier
+            .padding(20.dp)
+    ) {
+        val (illustration, card) = createRefs()
+        Box(
+            modifier = Modifier.fillMaxSize()
+                .constrainAs(card) {
+                    top.linkTo(illustration.bottom)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                    bottom.linkTo(parent.bottom)
+                }
         ) {
-            QuestionIllustration()
-            Text("Who invented water?")
+            ElevatedCard(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(bottom = 100.dp),
+                shape = RoundedCornerShape(20.dp),
+                colors = CardDefaults.cardColors(containerColor = Color.White),
+                elevation = CardDefaults.cardElevation(50.dp)
+                ){
+                Column(modifier = Modifier.padding(top = 80.dp, start = 10.dp, end = 10.dp)) {
+                    Text("NASA was invented in the military?", color = colors.onPrimary, fontSize = 25.sp, fontFamily = sonoFamily, fontWeight = FontWeight.Bold)
+                }
+            }
+        }
+        Box(
+            modifier = Modifier
+                .size(150.dp)
+                .constrainAs(illustration) {
+                    top.linkTo(parent.top)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                    width = Dimension.fillToConstraints
+                }
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.fall),
+                contentDescription = "Question Illustration"
+            )
+
         }
     }
 }
 
 @Composable
-fun QuestionIllustration(){
-    Row(modifier = Modifier
-        .fillMaxSize()
-        .padding(start = 20.dp),
-        horizontalArrangement = Arrangement.Start) {
-        Image(
-            painter = painterResource(id = R.drawable.fall),
-            contentDescription = "Question Illustration"
-        )
-    }
+fun QuestionIllustration() {
+
 }
